abbac6a03636629f8e6fff8c73afc015770ef93e
psbaswserver1 <psb.awsserver1@gmail.com>
2021-09-24 16:35:21 +0530

    initial commit

--- mc/settings.py
+++ mc/settings.py
@@ -76,23 +76,23 @@ WSGI_APPLICATION = 'mc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'clrrmsdb',
-        'USER': 'clarorms',
-        'PASSWORD': 'Clarorms1@3',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'clrrmsdb',
+#         'USER': 'clarorms',
+#         'PASSWORD': 'Clarorms1@3',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 
 # Password validation
